7f5cfa8bd47171216c67e86cfa876df15101e7db
issacevans <bellanovember1st@gmail.com>
2023-06-21 18:06:58 +0800

    0621

--- third_code.py
+++ third_code.py
@@ -1,32 +1,42 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+# 4. 操作控端or廳主端，使用開牌查詢搜尋某一張注單，印出注單資訊(不用到細單)
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-
-def make_webdriver() -> Chrome:
-    options = create_options()
-    # s = Service(ChromeDriverManager().install())
-    driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    return driver
-
-
-def create_options() -> Options:
-    options = Options()
-    # options.add_argument("--headless")
-    options.add_argument("--start-maximized")
-    return options
-
+from selenium.webdriver.common.keys import Keys
 
 if __name__ == '__main__':
-    url = "https://admin.vir777.net/login"
-    _driver = make_webdriver()
-    _driver.get(url=url)
-    lan_dropdown = WebDriverWait(_driver, 5).until(EC.presence_of_element_located(By.CLASS_NAME, "el-select_el-dropdown"))
-    # lan_dropdown = _driver.find_element(By.CLASS_NAME, "el-dropdown-link")
-    lan_dropdown.click()
+    option = ChromeOptions()
+    option.add_experimental_option("detach", True)
+    option.add_experimental_option('excludeSwitches', ['enable-automation'])
+    drive = webdriver.Chrome(options=option)
+    drive.get("https://admin.vir777.net/login")
+    drive.maximize_window()
+    # XPATH 定位語系下拉選單並點擊繁體
+    # lan_bar = WebDriverWait(drive, 10).until(
+    #     EC.presence_of_element_located((By.XPATH, "//div[@class='el-select el-dropdown']"))).click()
+    # lan_zh = WebDriverWait(drive, 10).until(
+    #     EC.presence_of_element_located((By.XPATH, "//*[@class='el-dropdown-menu el-popper lang-menu']/li[1]"))).click()
 
-    _driver.quit()
+    # account = WebDriverWait(drive, 10).until(
+    #     EC.presence_of_element_located((By.XPATH, "//*[@type='text']")))
+    # account.send_keys("qaevans")
+
+    # CSS 定位語系下拉選單並點擊繁體
+    WebDriverWait(drive, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "div.el-select.el-dropdown"))).click()
+    WebDriverWait(drive, 10).until(
+        EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, ".el-dropdown-menu.el-popper.lang-menu>:nth-child(1)"))).click()
+
+    account = WebDriverWait(drive, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "*[type='text']")))
+    account.send_keys("qaevans")
+
+    passwd = WebDriverWait(drive, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "*[type='password']")))
+    passwd.send_keys("evans11")
+    passwd.send_keys(Keys.ENTER)
+
+    WebDriverWait(drive, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "*[href='/game/betrecord_search/bet_query']"))).click()
